ffa905dfed2d1c728f3c45dc95caddff2294805c
ShyelSingh <ShyelSingh321@gmail.com>
2025-09-06 06:00:06 -0700

    removed reference to gemini api

--- backend/routes/server.py
+++ backend/routes/server.py
@@ -11,8 +11,6 @@ from openai import OpenAI
 
 load_dotenv()
 
-GEMINI_API = os.getenv("GEMINI_API_KEY")
-
 REDDIT_CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
 REDDIT_CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
 REDDIT_USER_AGENT = os.getenv("REDDIT_USER_AGENT")
