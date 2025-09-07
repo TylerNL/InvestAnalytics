c42cf161c5582404207fa859edff638db5ee97d8
TylerNL <tylerle@ucsb.edu>
2025-09-06 20:24:29 -0700

    Fixed duplicate front end stuff

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -6,7 +6,7 @@ const SignUp = () => {
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
     const [loading, setLoading] = useState("");
-
+    
     
     return (
         <div>
